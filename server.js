bb7a8819ec229610466edc1d558072edc1c1ce3e
Aditya Dixit <dixitaditya2001@gmail.com>
2023-05-20 21:32:29 +0530

    fixed commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import {app} from "./app.js"
 import { connectDB } from "./data/database.js"
 connectDB()
-const port = 3000
+const port = 4000
 
 app.listen(port, (req,res)=>{
     console.log(`The server is running on port ${port} in ${process.env.NODE_ENV} Mode`);
