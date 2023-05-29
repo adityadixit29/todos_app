b5c5bf9df1c701d0b0e53bf556d6f29b0510f495
Aditya Dixit <dixitaditya2001@gmail.com>
2023-05-29 23:50:17 +0530

    fixed-app

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ export const app = express();
 import {config} from "dotenv"
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middleware/error.js";
+import cors from "cors";
+
 config()
 //middlewares
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
 //routes
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/task", taskRouter)
 
+//using error middleware
 app.use(errorMiddleware);
 
 app.get('/',(req,res)=>{
